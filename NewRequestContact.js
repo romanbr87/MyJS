55581c038702dec878dba9154dead9d97f65a88d
Roman Braverman <75772227+romanbr87@users.noreply.github.com>
2022-03-04 04:26:01 +0200

    Update NewRequestContact.js

--- NewRequestContact.js
+++ NewRequestContact.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useState, useContext } from "react";
 import {
   TouchableOpacity,
   StyleSheet,
@@ -6,120 +6,90 @@ import {
   View,
   TextInput,
   ActivityIndicator,
-  Alert,
 } from "react-native";
-import { TransText } from "react-native-translation";
-import transFile from "../../TranslateFile";
-import { DataStorage } from "../../DataStorage";
-import ContactList from "./Contant/ContactList";
-import { mainBtn } from "../../StylesComponent";
-import postInformation from "../../PostInformation";
-import { axiosRequest } from "../../utils/functionsUtils";
-import Colors from "../../Colors";
-import urls from "../../../api/urls";
-import BG from "../../BG/BG";
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Button } from "react-native-paper";
-import Icon from "react-native-vector-icons/Ionicons";
-import * as Localization from 'expo-localization';
+import { TransText, getTranslation } from "react-native-translation";
+import transFile from "../../translate/TranslateFile";
+import ContactList from "./Details/ContactList";
+import { mainBtn } from "../../utils/StylesComponent";
+import Colors from "../../utils/Colors";
+import BG from "../BG/BG";
+import ContactsIcon from "../../../assets/svg/categories/ContactsIcon";
+import AppContext from "../../store/AppContext";
+import BackBtn from "../backBtn/BackBtn";
+import { observer } from "mobx-react";
+import TextComp from "../text/TextComp";
+import PopupWindow from "../modal/PopupWindow";
+import ScreenNames from "../../screens/ScreenNames";
+import TranslateFile from "../../translate/TranslateFile";
 
-const locale = Localization.locale 
-const side = lovale === 'he-IL' ? "right" : "left";
-
-export default function NewRequestContact(props) {
+const NewRequestContact = observer((props) => {
+  const { postId, NewRequestStore, updateUserMessage } = props
+  const postData = NewRequestStore.postData
   //true for my details radio and false for someone else radio
   const [value, setValue] = useState(true);
-  const [userData, setUserData] = useContext(DataStorage);
-  //checker for the input value (first with my phone, second empty)
-  const [inputFlag, setInputFlag] = useState(true);
-  const [userName, setUserName] = useState(
-    props.postDataToServer.userName ? props.postDataToServer.userName : ""
-  );
-  const [phone, setPhone] = useState(props.postDataToServer.phone);
+  const [userName, setUserName] = useState(postData.userName ? postData.userName : "");
+  const [phone, setPhone] = useState(postData.phone);
   const [isLoading, setIslLoading] = useState(false);
   const [showContactsView, setShowContactsView] = useState(false);
   const [showContactsViewButton, setShowContactsViewButton] = useState(false);
-  const postData = props.postDataToServer;
-  const nowTime = new Date().getTime();
-  const [postContainer, setPostContainer] = useState({
-    lat: postData.lat,
-    lng: postData.lng,
-    phone: postData.phone,
-    expireDate: postInformation.expireDate[postData.expireDate].value + nowTime,
-    content: postData.content,
-    userName: postData.userName ? postData.userName : "",
-    radius: postInformation?.radius[postData.radius]?.value,
-    cityId: postData.city,
-    attachments: [],
-    categoryType: postData.categoryId,
-    urgencyType: postData?.urgencyId?.value,
-  });
-  const activePageFlag = props.activePage;
+  const [isPhoneNumber, setIsPhoneNumber] = useState (true);
+  const [isUsername, setIsUername] = useState (true);
   const navigation = props.navigation;
+  const [isVisible, setisVisible] = useState(false);
+  const { user } = useContext(AppContext);
+  const { getDirection, deviceLanguage } = user;
+  const direction = getDirection (deviceLanguage);
+  
+  //{"align": "right", "alignSelf": "flex-end", "flexDirection": "row-reverse"}
 
-  // ------ axios request --------//
-  const sendPostToServer = async (images, editOrNew) => {
-    if (activePageFlag == 2) {
-      const postImages = images ? images : [];
-      const mainUrl = userData.mainUrl;
-      let sentPostDomain = "";
-      if (editOrNew == false) {
-        sentPostDomain = urls.createNewPost(postData.token);
-      } else {
-        sentPostDomain = urls.editPostRequest(postData.id, postData.token);
-      }
-      let fullPostDataToServer = postContainer;
-      fullPostDataToServer.attachments = postImages;
-      console.log("data sent to server:", fullPostDataToServer);
-      let data = axiosRequest(sentPostDomain, "POST", fullPostDataToServer);
-      // checking the data Promise recieved by the function
-      data
-        .then(async (serverData) => {
-          console.log("serverData", serverData);
-          let newUser = { ...userData.userDetails };
-          newUser.userName = userName;
-          AsyncStorage.setItem("USER", JSON.stringify(newUser));
-          setUserData({
-            ...userData,
-            updateUserMessage: transFile.newRequest_successMessage,
-            userDetails: { ...userData.userDetails, userName: userName },
-            flag: userData.flag + 1,
-          });
-          // await props.setActivePage(0);
-          setIslLoading(false);
-          navigation.navigate("homePageMain");
-        })
-        .catch((mes) => {
-          setIslLoading(false);
-          console.log("error axios line 72 newRequestContact", mes);
-          Alert.alert(mes);
-        });
-    }
-  };
-  const sendImageAndPostToServer = async (editOrNew) => {
-    let imagesLength = props.postDataToServer.image?.length;
-    if (imagesLength > 0) {
-      const url =
-        props.userData.mainUrl +
-        props.userData.urlType.imageUplaod +
-        `token=${props.postDataToServer.token}`;
-      let imageToServer = axiosRequest(
-        url,
-        "post",
-        props.postDataToServer.image
-      );
-      imageToServer
-        .then((dataFromServer) => {
-          console.log("axios image", dataFromServer);
-          sendPostToServer(dataFromServer, editOrNew);
-        })
-        .catch((error) => console.log("error uploading pic", error));
-    } else {
-      console.log("no pic");
-      sendPostToServer(undefined, editOrNew);
-    }
-  };
+  const renderContent = () => {
+    return (
+      <View style={{...styles.detailsBox}}>
+        <View style={{...styles.textInputTitleView, flexDirection: direction.flexDirection}}>
+          <TransText
+            style={styles.inputsTitlesSmall}
+            dictionary={transFile.name}
+          />
+          { !isUsername && <TransText
+            style={{...styles.inputsTitlesSmall, color: Colors.red, 
+            }}
+            dictionary={transFile.validation_shortName}
+          /> }
+        </View>
+        <TextInput
+          value={userName}
+          onChangeText={(text) => handleNameInput(text)}
+          style={{...styles.secondInput,
+          borderColor: isUsername ? Colors.colorBlack : Colors.red }}
+          textAlign={direction.align}
+          keyboardType="default"
+          editable={!value}
+        ></TextInput>
 
+        {/* inputs small titles */}
+        <View style={{...styles.textInputTitleView, flexDirection: direction.flexDirection}}>
+          <TransText
+            style={styles.inputsTitlesSmall}
+            dictionary={transFile.phone}
+          />
+          { !isPhoneNumber && <TransText
+            style={{...styles.inputsTitlesSmall, color: Colors.red, }}
+            dictionary={transFile.validation_validNumber}
+          /> }
+        </View>
+        <TextInput
+          value={phone}
+          onChangeText={(text) => handlePhoneInput(text)}
+          style={{...styles.secondInput,
+          borderColor: isPhoneNumber ? Colors.colorBlack : Colors.red }}
+          textAlign={direction.align}
+          keyboardType="number-pad"
+          editable={!value}
+          dataDetectorTypes="phoneNumber"
+        ></TextInput>
+      </View>
+    )
+  }
   //----radio props for tuggle details------
   var radio_props = [
     {
@@ -133,30 +103,7 @@ export default function NewRequestContact(props) {
       ),
       value: true,
       content: (
-        <View style={styles.detailsBox}>
-          <TransText
-            style={[styles.inputsTitlesSmall]}
-            dictionary={transFile.name}
-          />
-          <TextInput
-            value={userName}
-            editable={true}
-            onChangeText={(text) => inputsChangeHandler("userName", text)}
-            style={styles.secondInput}
-            keyboardType="default"
-          ></TextInput>
-          <TransText
-            style={styles.inputsTitlesSmall}
-            dictionary={transFile.phone}
-          />
-          <TextInput
-            value={phone}
-            editable={false}
-            onChangeText={(text) => inputsChangeHandler("phone", text)}
-            style={styles.secondInput}
-            keyboardType="default"
-          ></TextInput>
-        </View>
+        renderContent()
       ),
     },
     {
@@ -169,39 +116,43 @@ export default function NewRequestContact(props) {
         />
       ),
       content: (
-        <View style={styles.detailsBox}>
-          <TransText
-            style={styles.inputsTitlesSmall}
-            dictionary={transFile.name}
-          />
-          <TextInput
-            value={userName}
-            onChangeText={(text) => inputsChangeHandler("userName", text)}
-            style={styles.secondInput}
-            keyboardType="default"
-          ></TextInput>
-
-          {/* inputs small titles */}
-          <TransText
-            style={styles.inputsTitlesSmall}
-            dictionary={transFile.phone}
-          />
-          <TextInput
-            value={phone}
-            onChangeText={(text) => inputsChangeHandler("phone", text)}
-            style={styles.secondInput}
-            keyboardType="number-pad"
-          ></TextInput>
-        </View>
+        renderContent()
       ),
       value: false,
     },
   ];
 
-  const nextBtnAction = async () => {
-    setIslLoading(true);
-    sendImageAndPostToServer(props.editOrNew);
+  const phoneNumberValidator = (phoneNumber) => {
+    let phoneRegex = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+    let validation = phoneNumber.match(phoneRegex);
+    return validation!==null;
+  }
+
+  const sendRequetToServer = () => {
+    NewRequestStore.sendPostToServer(postId).then((res) => {     
+      if (res.code === 200) {
+        updateUserMessage(TranslateFile.newRequest_successMessage);
+        navigation.navigate(ScreenNames.homePageMain)
+      }
+      else {
+        updateUserMessage(TranslateFile.serverConnectionErrorMessage);
+        navigation.navigate(ScreenNames.homePageMain)
+      }
+    }).catch(e => {
+      updateUserMessage(TranslateFile.serverConnectionErrorMessage);
+      navigation.navigate(ScreenNames.homePageMain)
+    })
   };
+
+  const nextBtnAction = () => {
+      if (value === radio_props[1].key) 
+      { 
+        if (isPhoneNumber && isUsername) {
+          setisVisible (true);
+        }
+      }
+      else sendRequetToServer ();
+  }    
 
   const showTheContactsView = () => {
     setShowContactsView(!showContactsView);
@@ -213,56 +164,77 @@ export default function NewRequestContact(props) {
 
   const userNameHandler = (trueFalse) => {
     if (trueFalse) {
-      setUserName(props.postDataToServer.userName);
-      setPhone(props.postDataToServer.phone);
+      handleNameInput(postData.userName);
+      handlePhoneInput(postData.phone);
     } else {
-      setUserName("");
-      setPhone("");
+      handleNameInput("");
+      handlePhoneInput("");
     }
   };
 
   //updates the user and phone and close the modal
   const pickedContectHandler = (obj) => {
-    setUserName(obj.name);
-    setPhone(obj.phone);
+    handleNameInput (obj.name);
+    handlePhoneInput (obj.phone);
     setShowContactsViewButton(true);
     setShowContactsView(!showContactsView);
   };
+  const handleNameInput = (text) => {
+    setIsUername (text?.length > 0);    
+    setUserName(text);
+  }
+  const handlePhoneInput = (text) => {
+    setIsPhoneNumber (phoneNumberValidator (text))
+    setPhone(text);
+  }
 
-  //inputs change and validation
-  const inputsChangeHandler = (field, text) => {
-    if (field === "userName") {
-      console.log("text", text);
-      setPostContainer({ ...postContainer, userName: text });
-      setUserName(text);
-    } else {
-      setPhone(text);
-    }
-  };
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => {
+        return (
+          <BackBtn onPress={() => {
+            props.setActivePage((prev) => prev - 1);
+          }} />
+        );
+      },
+    });
+  }, [navigation]);
+  
+  
+  const sendToWhatsappMessageParams = {
+    title: transFile.requestDetails_whatsApp,
+    text: getTranslation(transFile.whatsAppQuestion),
+    noBtn:{
+      onPress: () => {
+        setisVisible(false);
+        sendRequetToServer();
+      },
+      text: transFile.cancel_uppercase,
+    },
+    yesBtn:{
+      onPress: () =>{ 
+        setisVisible(false);
+      },
+      text: transFile.send,
+    },
+    isVisible: isVisible
+  }
+
   //-------------------------------------------------------------------------
 
   return (
-    // <KeyboardAvoidingView behavior="padding" style={{ flex: 1,  height:sizes.PageHieght - 250,  }}>
     <BG style={styles.container}>
-      {/* <View style={styles.container}> */}
       {/* top text */}
-      <View style={styles.textView}>
-        <Text style={styles.explain}>
-          <TransText dictionary={transFile.newRequest_contactDescription} />
-        </Text>
-      </View>
-
+      <TextComp style={styles.textView} alignText={true} translate={transFile.newRequest_contactDescription} />
       <View>
         {/* /my details radio */}
         <View>
-          <View style={styles.detailsRow}>
+          <View style={{...styles.detailsRow, 
+              direction: direction.align == "left" ? "ltr" : "rtl",}}>
             <Text style={styles.radioText}>{radio_props[0].label}</Text>
-
             <TouchableOpacity
               style={styles.radioCircle}
               onPress={() => {
-                setInputFlag(1);
-                setInputFlag(radio_props[0].key);
                 setValue(radio_props[0].key);
                 userNameHandler(true);
                 showTheContactsViewButton(false);
@@ -273,7 +245,6 @@ export default function NewRequestContact(props) {
               )}
             </TouchableOpacity>
           </View>
-
           {/* name and phone input */}
           <View style={styles.inputs}>
             {value === radio_props[0].key && (
@@ -283,10 +254,10 @@ export default function NewRequestContact(props) {
             )}
           </View>
         </View>
-
         <View>
           {/* someone else radio */}
-          <View style={styles.detailsRow}>
+          <View style={{...styles.detailsRow, 
+              direction: direction.align == "left" ? "ltr" : "rtl",}}>
             <Text style={styles.radioText}>{radio_props[1].label}</Text>
 
             <TouchableOpacity
@@ -314,19 +285,34 @@ export default function NewRequestContact(props) {
       </View>
       <View>
         {/* page for picking other contact */}
-        { showContactsViewButton && (
-          <Button 
+        {showContactsViewButton && (
+          <TouchableOpacity
             onPress={showTheContactsView}
-            style={styles.btnContacts}>
-                
-            <Icon name="rocket" size={12} color='#747474' />
-            <Text> 
-              <TransText dictionary={transFile.newRequest_contactsPicker} 
-                style={{textTransform: 'none', color: '#747474' }}/>
-            </Text>
-          </Button>
-          )}  
-                
+            style={styles.btnContacts}
+          >
+            <View
+              style={{ flexDirection: direction.flexDirection, alignItems: 'center' }}
+            >
+              <ContactsIcon />
+              <Text
+                style={{
+                  justifyContent: "center",
+                  marginHorizontal: 5,
+                }}
+              >
+                <TransText
+                  dictionary={transFile.newRequest_contactsPicker}
+                  style={{
+                    color: "#747474",
+                    fontSize: 18,
+                    textTransform: "none",
+                  }}
+                />
+              </Text>
+            </View>
+          </TouchableOpacity>
+        )}
+
         {showContactsView && (
           <ContactList
             pickedContectHandler={pickedContectHandler}
@@ -342,11 +328,12 @@ export default function NewRequestContact(props) {
           mainBtn(nextBtnAction, transFile.next_uppercase)
         )}
       </View>
-      {/* </View> */}
-      {/* </KeyboardAvoidingView> */}
+      {
+        !value && <PopupWindow {...sendToWhatsappMessageParams} />
+      }
     </BG>
   );
-}
+})
 
 //-------------styles-------------------------------------
 const styles = StyleSheet.create({
@@ -357,8 +344,7 @@ const styles = StyleSheet.create({
   detailsRow: {
     flexDirection: "row-reverse",
     justifyContent: "flex-end",
-    direction: "rtl",
-    paddingRight: 20,
+    paddingHorizontal: 20,
     paddingTop: 10,
   },
 
@@ -377,7 +363,6 @@ const styles = StyleSheet.create({
   detailsBox: {
     flex: 1,
     width: "100%",
-    alignItems: "flex-start",
     borderColor: "#ccc",
     borderRadius: 5,
     paddingLeft: 10,
@@ -392,8 +377,8 @@ const styles = StyleSheet.create({
     borderColor: "rgb(57,55,119)",
     alignItems: "center",
     justifyContent: "center",
-    marginHorizontal: 10,
-    marginLeft: "-2.5%",
+    marginLeft: -10,
+    marginHorizontal: 5,
   },
 
   selectedRb: {
@@ -421,21 +406,13 @@ const styles = StyleSheet.create({
     fontSize: 18,
     paddingHorizontal: 16,
     width: "100%",
-    textAlign: "left",
     marginVertical: 10,
     color: "grey",
   },
 
-  explain: {
-    width: "80%",
-    textAlign: "left",
-  },
-
   textView: {
     width: "100%",
-    alignItems: side === 'left' ? "flex-start" : "flex-end",
-    justifyContent: side === 'left' ? "flex-start" : "flex-end",
-    marginLeft: "2%",
+    paddingHorizontal: 10
   },
 
   btnBox: {
@@ -443,7 +420,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     alignSelf: "center",
     alignContent: "center",
-    justifyContent: "flex-end",
     marginVertical: 36,
     width: 350,
   },
@@ -454,15 +430,21 @@ const styles = StyleSheet.create({
 
   radioTitles: {
     color: "rgb(116,116,116)",
-    fontWeight: "normal"
+    fontWeight: "normal",
+  },
+
+  textInputTitleView: {
+    alignItems: "center",
+    height: 29,
+    width: "100%",
+    justifyContent: "space-between",
   },
 
   btnContacts: {
-    alignItems: "flex-start",
-    justifyContent: "flex-start",
-    color: '#747474',
-    fontWeight: "normal",
-    textTransform: 'none',
-    height: 47,
+    fontSize: 18,
+    height: 50,
+    marginHorizontal: 10,
   },
 });
+
+export default NewRequestContact
